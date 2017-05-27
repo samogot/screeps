eae8f83b2f488c9ead86b3cff3176a4a29650bc9
samogot <samogot@gmail.com>
2017-05-27 12:00:05 +0300

    fix bugs

--- dist/main.js
+++ dist/main.js
@@ -13,7 +13,7 @@ module.exports.loop = function () {
         if (structure.structureType === STRUCTURE_TOWER) {
             var closestHostile = structure.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
             if (closestHostile) {
-                tower.attack(closestHostile);
+                structure.attack(closestHostile);
             }
 
             var closestDamagedStructure = structure.pos.findClosestByRange(FIND_STRUCTURES, {
@@ -27,13 +27,13 @@ module.exports.loop = function () {
 
     var capacity = Game.rooms['W31S91'].energyCapacityAvailable;
     var rules = {
-        harvester: {
-            count: 2,
-            body: [...Array(Math.min(Math.floor((capacity - 50) / 100), 5)).fill(WORK), MOVE]
-        },
         mover: {
             count: 1,
             body: [...Array(Math.floor(capacity / 100)).fill(CARRY), ...Array(Math.floor(capacity / 100)).fill(MOVE)]
+        },
+        harvester: {
+            count: 2,
+            body: [...Array(Math.min(Math.floor((capacity - 50) / 100), 5)).fill(WORK), MOVE]
         },
         dirrectUpgrader: {
             count: 2,
@@ -50,7 +50,7 @@ module.exports.loop = function () {
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
         for (let role of roles) {
-            if (creep.memory.role == role.name) {
+            if (creep.memory.role === role.name) {
                 role.run(creep);
             }
         }
